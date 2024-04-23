405d0e3d90b24a708d021cd4eb1824d62a0275a8
elya-le <elyaj.le@gmail.com>
2024-04-23 19:11:58 -0400

    complete testing User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+const { restoreUser } = require('../../utils/auth.js');// GET /api/restore-user middleware
+router.use(restoreUser);
+
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
@@ -16,5 +19,21 @@ router.get('/set-token-cookie', async (_req, res) => {
   setTokenCookie(res, user);
   return res.json({ user: user });
 });
+
+// GET /api/restore-user middleware
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+
+
 
 module.exports = router;
