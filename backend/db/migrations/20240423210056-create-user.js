668cbffe57d346bf518b2c6042b67f2e47fefb0b
elya-le <elyaj.le@gmail.com>
2024-04-23 17:43:10 -0400

    updated db migration and seeder files and corrected location of .env file

---
+++ backend/db/migrations/20240423210056-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let options = {}; // initializes empty object to store options that may be needed for the migration
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) { // defines 'down' method for reverting changes made by the up method
+    options.tableName = "Users"; // specifies the table name in the 'options' object for targeting the correct table
+    return queryInterface.dropTable(options); // asynchronously drops 'Users' table using the provided options
+  }
+};
