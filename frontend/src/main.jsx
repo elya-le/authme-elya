606eed090039b0c9e890c703764c4d3bdcc7e67c
elya-le <elyaj.le@gmail.com>
2024-05-17 13:19:00 -0500

    set up and test CSRF protection for fetch requests

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // note the updated import
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
