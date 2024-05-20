2dbfee7a14ee5cdc49cc5550b460436c8e489851
elya-le <elyaj.le@gmail.com>
2024-05-20 14:15:03 -0500

    test the redux store setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;  // make sure this line is correctly placed and executed
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
