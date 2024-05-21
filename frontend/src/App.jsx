c1ac0acaa037f844c6a934222db6f76643e0a29f
elya-le <elyaj.le@gmail.com>
2024-05-20 20:36:50 -0500

    convert login and signup forms to modals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+// If you have converted SignupFormPage to SignupFormModal, update the import as follows:
+import SignupFormModal from './components/SignupFormModal/SignupFormModal';
 import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
 
@@ -11,14 +11,16 @@ function Layout() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true);
+    });
   }, [dispatch]);
 
   return (
     <>
-    <Navigation isLoaded={isLoaded} />
-    {isLoaded && <Outlet />}
-  </>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
   );
 }
 
@@ -28,15 +30,11 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1>heddo</h1>
+        element: <h1>Welcome!</h1>
       },
       {
-        path: '/login',
-        element: <LoginFormPage />
-      },
-      {
-        path: '/signup',
-        element: <SignupFormPage />
+        path: 'signup',
+        element: <SignupFormModal /> 
       }
     ]
   }
