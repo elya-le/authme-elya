54986110bcdf206aa648b269578777a125f15745
elya-le <elyaj.le@gmail.com>
2024-05-20 19:22:21 -0500

    feat: implement signup functionality with session actions, reducer, and SignupFormPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -9,9 +10,7 @@ function Layout() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true);
-    });
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
   return (
@@ -27,11 +26,15 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1>Welcome!</h1>
+        element: <h1>heddo</h1>
       },
       {
         path: '/login',
         element: <LoginFormPage />
+      },
+      {
+        path: '/signup',
+        element: <SignupFormPage />
       }
     ]
   }
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -49,6 +49,23 @@ export const logout = () => async (dispatch) => {
     return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user)); // set the session user with the response
+    return response;
+};
+
 const sessionReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER:
