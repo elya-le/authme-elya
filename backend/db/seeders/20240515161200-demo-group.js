e7938ab8f19b973bc03191ccf3ebb7f36d31bdb6
elya-le <elyaj.le@gmail.com>
2024-05-23 13:28:50 -0500

    update seeder file

--- backend/db/seeders/20240515161200-demo-group.js
+++ backend/db/seeders/20240515161200-demo-group.js
@@ -10,7 +10,7 @@ module.exports = {
       { tableName: 'Groups', schema: options.schema },
       [
         {
-          organizerId: 2, 
+          organizerId: 1, 
           name: 'Urban Trailblazers',
           about: "A group for city-dwelling dogs and their owners who love to explore urban landscapes and parks.",
           type: 'In person',
@@ -21,7 +21,7 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          organizerId: 2, 
+          organizerId: 1, 
           name: 'Beachfront Barks',
           about: "Perfect for dogs who cant stay away from the water. Meetups include beach games and swimming sessions.",
           type: 'In person',
@@ -32,7 +32,7 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          organizerId: 2, 
+          organizerId: 1, 
           name: 'Big Pup',
           about: "Help your pony-sized lapdog socialize and make friends with other pups they won't accidentally flatten",
           type: 'In person',
@@ -43,7 +43,7 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          organizerId: 2,
+          organizerId: 1,
           name: 'Shih Sass Tzu',
           about: "Is your Shih Tzu a little Napoleon? Perfect! Our group loves those with a 'short' temper but a big heart",
           type: 'In person',
