e3bb9800a2321dbbe2aa0cd3375286d65d5e92a9
elya-le <elyaj.le@gmail.com>
2024-05-24 16:04:20 -0500

    feat:hard refresh on demo-userand logout button

---
+++ backend/db/seeders/20240515161100-demo-user.js
@@ -0,0 +1,56 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      { tableName: 'Users', schema: options.schema },
+      [
+        {
+          firstName: 'Demo',
+          lastName: 'User',
+          email: 'demo@user.io',
+          username: 'Demo-User',
+          hashedPassword: bcrypt.hashSync('password'),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          firstName: 'Demo',
+          lastName: 'User2',
+          email: 'demo2@user.io',
+          username: 'Demo-User2',
+          hashedPassword: bcrypt.hashSync('password2'),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          firstName: 'Demo',
+          lastName: 'User3',
+          email: 'demo3@user.io',
+          username: 'Demo-User3',
+          hashedPassword: bcrypt.hashSync('password3'),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete(
+      { tableName: 'Users', schema: options.schema },
+      {
+        username: {
+          [Sequelize.Op.in]: ['Demo-User', 'Demo-User2', 'Demo-User3'],
+        },
+      }
+    );
+  },
+};
