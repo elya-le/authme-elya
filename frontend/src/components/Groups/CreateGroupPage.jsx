d2ae80b3c0b45d8edc115e3128949b99b5e3f595
elya-le <elyaj.le@gmail.com>
2024-05-29 15:58:48 -0500

    update feature: margins on update and create group

--- frontend/src/components/Groups/CreateGroupPage.jsx
+++ frontend/src/components/Groups/CreateGroupPage.jsx
@@ -4,9 +4,11 @@ import CreateGroupForm from './CreateGroupForm';
 const CreateGroupPage = () => {
   return (
     <div className='create-group-page'>
+      <div className='create-group-header'>
       <p>BECOME AN ORGANIZER</p><br></br>
       <h2>We&apos;ll walk you through a few steps to build your local community</h2>
       <hr></hr>
+      </div>
       <CreateGroupForm />
     </div>
   );
