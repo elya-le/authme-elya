2a32e20db1071270e974997b97b20e952d38a5ff
elya-le <elyaj.le@gmail.com>
2024-05-29 12:42:59 -0500

    update feature: add placeholder image to group card on landing page

--- frontend/src/components/Groups/GroupCard.jsx
+++ frontend/src/components/Groups/GroupCard.jsx
@@ -2,21 +2,31 @@ import { Link } from 'react-router-dom';
 import './GroupCard.css';
 
 const GroupCard = ({ group }) => {
-  // assuming each group will have and array og images we take the first one
+  // assuming each group will have an array of images, we take the first one
   const imageUrl = group.GroupImages && group.GroupImages.length > 0
     ? group.GroupImages[0].url
     : '/public/images/img.png'; 
-    return (
-      <div className='group-card'>
-        <Link to={`/groups/${group.id}`}>
-          <img src={imageUrl} alt={group.name} className='group-card-image' />
-          <div className="group-card-content">
-            <h3>{group.name}</h3>
-            <p>{group.about}</p>
-          </div>
-        </Link>
-      </div>
-    );
+
+  const handleImageError = (e) => {
+    e.target.src = '/public/images/img.png'; // set to placeholder image if loading fails
+  };
+
+  return (
+    <div className='group-card'>
+      <Link to={`/groups/${group.id}`}>
+        <img 
+          src={imageUrl} 
+          alt={group.name} 
+          className='group-card-image' 
+          onError={handleImageError} 
+        />
+        <div className="group-card-content">
+          <h3>{group.name}</h3>
+          <p>{group.about}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default GroupCard;
